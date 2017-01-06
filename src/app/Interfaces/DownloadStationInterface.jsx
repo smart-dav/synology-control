8696b8b52aa385eaae07afbe4bf01a3e4d5094df
amiceli <amiceli@mailinblack.com>
2017-01-06 19:54:47 +0100

    Add react router.

---
+++ src/app/Interfaces/DownloadStationInterface.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class DownloadStationInterface extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    Download station
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default DownloadStationInterface;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import {render} from 'react-dom';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import {Router, Route, Link, browserHistory, hashHistory} from 'react-router';
+import AppBar from 'material-ui/AppBar';
 
-
+import AppMenu from './AppMenu.jsx'
 import MainVue from './MainVue.jsx'
+import DownloadStationInterface from './Interfaces/DownloadStationInterface.jsx'
 
 injectTapEventPlugin();
 
 require("../less/App.less");
 
 class App extends React.Component {
-    render () {
+    render() {
         return (
-            <div>
-                <MuiThemeProvider>
-                    <MainVue />
-                </MuiThemeProvider>
-            </div>
+            <MuiThemeProvider>
+                <div>
+                    <AppBar className="AppBar" iconElementLeft={<AppMenu/>}/>
+                    <br/>
+                    <Router history={hashHistory}>
+                        <Route path="/" component={MainVue}/>
+                        <Route path="/downloadStation" component={DownloadStationInterface}/>
+                    </Router>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
