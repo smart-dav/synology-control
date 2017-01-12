757fb83e3345f769e95d3db94e9bd1db0600c17f
amiceli <miceliantoine@protonmail.com>
2017-01-12 22:30:47 +0100

    Add more menu on list item.

--- src/app/Interfaces/List/TaskListInterface.jsx
+++ src/app/Interfaces/List/TaskListInterface.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
+import NavigationCheck from 'material-ui/svg-icons/navigation/check';
+import {grey400} from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+const iconButtonElement = (
+    <IconButton
+        touch={true}
+        tooltip="more"
+        tooltipPosition="bottom-left"
+    >
+        <MoreVertIcon color={grey400}/>
+    </IconButton>
+);
+
 
 class TaskListInterface extends React.Component {
 
@@ -9,9 +26,33 @@ class TaskListInterface extends React.Component {
         this.state = {tasks: props.tasks};
     }
 
+    onDelete(taskId) {
+        console.log("on delete", taskId);
+    }
+
+    onPause(taskId) {
+        console.log("on pause", taskId);
+    }
+
+    getRightIcon(taskId) {
+        return (
+            <IconMenu iconButtonElement={iconButtonElement}>
+                <MenuItem onTouchTap={this.onDelete.bind(this, taskId)}>Delete</MenuItem>
+                <MenuItem onTouchTap={this.onPause.bind(this, taskId)}>Pause</MenuItem>
+            </IconMenu>
+        );
+    }
+
+
     getTaskListChild() {
+        let self = this;
+
+
         return this.state.tasks.map(function (task, index) {
-            return <ListItem primaryText={task.title} key={task.id}/>
+            return <ListItem primaryText={task.title} key={task.id}
+                             rightIconButton={self.getRightIcon(task.id)}
+                             leftIcon={<NavigationCheck />}
+            />
         })
     }
 
